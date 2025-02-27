9a5542e72282e90d7582d52d7fabf44b564867ca
erc83 <espinoza.eric@gmail.com>
2025-02-27 10:40:48 -0300

    add: config envs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { Logger } from '@nestjs/common'
+import { envs } from './config'
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
+  const logger = new Logger('Order-MS')
+
   const app = await NestFactory.create(AppModule);
+
   await app.listen(process.env.PORT ?? 3000);
+
+  logger.log(`Order Microservice running on port ${ envs.port }`)
+
 }
 bootstrap();
