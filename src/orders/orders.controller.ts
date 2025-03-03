66631354a020c2c624fcd9aca8155b015e25a6f4
erc83 <espinoza.eric@gmail.com>
2025-03-03 14:44:53 -0300

    del: clean resource orders

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -2,7 +2,6 @@ import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
 
 @Controller()
 export class OrdersController {
@@ -23,13 +22,4 @@ export class OrdersController {
     return this.ordersService.findOne(id);
   }
 
-  @MessagePattern('updateOrder')
-  update(@Payload() updateOrderDto: UpdateOrderDto) {
-    return this.ordersService.update(updateOrderDto.id, updateOrderDto);
-  }
-
-  @MessagePattern('removeOrder')
-  remove(@Payload() id: number) {
-    return this.ordersService.remove(id);
-  }
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -16,11 +16,4 @@ export class OrdersService {
     return `This action returns a #${id} order`;
   }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} order`;
-  }
 }
