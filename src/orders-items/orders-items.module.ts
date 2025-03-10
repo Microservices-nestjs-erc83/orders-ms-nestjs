608cbf062c076dfe5ddeb8074023cbec463b7adf
erc83 <espinoza.eric@gmail.com>
2025-03-10 16:30:40 -0300

    add: custom nats.module.ts

--- src/orders-items/orders-items.module.ts
+++ src/orders-items/orders-items.module.ts
@@ -3,21 +3,13 @@ import { OrdersItemsService } from './orders-items.service';
 import { OrdersItemsController } from './orders-items.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { envs, NATS_SERVICE, PRODUCT_SERVICE } from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   controllers: [OrdersItemsController],
   providers: [OrdersItemsService],
   imports:[
-    ClientsModule.register([
-      {
-        name: NATS_SERVICE,
-        transport: Transport.NATS,
-        options: {
-          //host: envs.productsMicroserviceHost,
-          //port: envs.productsMicroservicePort,
-        }
-      }
-    ])
+    NatsModule,
   ]
 })
 export class OrdersItemsModule {}
